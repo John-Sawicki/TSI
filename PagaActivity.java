fd0f2b9e56be56e26ca7a8af772c1ab241695bd4
john-sawicki <3Gxapnil>
2018-09-11 21:37:29 -0500

    set up changing colors for paga circles

--- PagaActivity.java
+++ PagaActivity.java
@@ -3,6 +3,7 @@ package com.example.android.tsi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -10,20 +11,45 @@ import android.widget.TextView;
 import butterknife.BindView;
 
 public class PagaActivity extends AppCompatActivity {
-    @BindView(R.id.im_background)FrameLayout mFrameBackground;
-    @BindView(R.id.im_circle1) ImageView ivCircle1;
-    @BindView(R.id.im_circle2)ImageView ivCircle2;
-    @BindView(R.id.im_circle3)ImageView ivCircle3;
-    @BindView(R.id.im_circle4)ImageView ivCircle4;
-    @BindView(R.id.im_circle5)ImageView ivCircle5;
+
+    FrameLayout mFrameBackground;
+    ImageView ivCircle1, ivCircle2, ivCircle3, ivCircle4, ivCircle5;
+    TextView tv_db_level1, tv_distance1, tv_db_level2, tv_distance2, tv_db_level3, tv_distance3, tv_db_level4, tv_distance4, tv_db_level5, tv_distance5, tv_db_level6, tv_distance6;
+    Button btn_calculate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paga);
         View viewCircle = findViewById(R.id.lo_top_right);
         View viewDb = findViewById(R.id.lo_bottom_left);
+        mFrameBackground = viewCircle.findViewById(R.id.im_background);
+        ivCircle1= viewCircle.findViewById(R.id.im_circle1);
+        ivCircle2= viewCircle.findViewById(R.id.im_circle2);
+        ivCircle3= viewCircle.findViewById(R.id.im_circle3);
+        ivCircle4= viewCircle.findViewById(R.id.im_circle4);
+        ivCircle5= viewCircle.findViewById(R.id.im_circle5);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level1);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance1);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level2);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance2);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level3);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance3);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level4);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance4);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level5);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance5);
+        tv_db_level1 = viewDb.findViewById(R.id.tv_db_level6);
+        tv_distance1 = viewDb.findViewById(R.id.tv_distance6);
+        btn_calculate = viewDb.findViewById(R.id.btn_calculate);
+        btn_calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO change background color
+                //ivCircle5.setBackgroundColor(getResources().getColor(R.color.circle_8_Red));
+                ivCircle5.setColorFilter(getResources().getColor(R.color.circle_8_Red));
 
+            }
 
-
+        });
     }
 }
